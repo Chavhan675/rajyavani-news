497771dc417f60d689cb318efa312585586a6605
Akash Chavhan <2023bcs072@sggs.ac.in>
2026-03-08 02:08:53 +0530

    fix sitemap build error

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,45 +1,7 @@
 import axios from "axios"
 
-const API="http://localhost:5000/api"
+const API = axios.create({
+  baseURL: "https://rajyavani-news.onrender.com/api"
+})
 
-export const getLatestNews=async()=>{
-
-const res=await axios.get(`${API}/news/latest`)
-return res.data
-
-}
-
-export const getTrendingNews=async()=>{
-
-const res=await axios.get(`${API}/news/trending`)
-return res.data
-
-}
-
-export const getBreakingNews=async()=>{
-
-const res=await axios.get(`${API}/news/breaking`)
-return res.data
-
-}
-
-export const getFeaturedNews=async()=>{
-
-const res=await axios.get(`${API}/news/featured`)
-return res.data
-
-}
-
-export const getNewsBySlug=async(slug)=>{
-
-const res=await axios.get(`${API}/news/${slug}`)
-return res.data
-
-}
-
-export const getNewsByCategory=async(slug)=>{
-
-const res=await axios.get(`${API}/news/category/${slug}`)
-return res.data
-
-}
+export default API
