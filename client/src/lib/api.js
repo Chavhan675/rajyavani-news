60fc40a576ce58b97993605a74a5525f6bd23b3e
Akash Chavhan <2023bcs072@sggs.ac.in>
2026-03-08 02:26:24 +0530

    fix sitemap build error

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "https://rajyavani-news.onrender.com/api"
+  baseURL: "https://rajyavani-news.onrender.com"
 })
 
 export default API
